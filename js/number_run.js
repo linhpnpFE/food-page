3b0ba2629c3175e1b4ae06380f1aefd6ddd19de5
PhamBaVan <bavanpham2000@gmail.com>
2022-07-25 16:41:21 +0700

    floral, doing 16:41 25/07/2022

--- js/number_run.js
+++ js/number_run.js
@@ -12,18 +12,23 @@ function animateNumber(finalNumber, delay, startNumber = 0, callback) {
   }
   
   document.addEventListener('DOMContentLoaded', function () {
-    animateNumber(4000000, 1, 0, function (number) {
-      const formattedNumber = number
-      document.getElementById('transaction-count').innerText = formattedNumber
+    animateNumber(3000, 10, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('package-sold-number').innerText = formattedNumber
     })
     
-    animateNumber(98, 50, 0, function (number) {
-      const formattedNumber = number
-      document.getElementById('city-count').innerText = formattedNumber
+    animateNumber(5000, 10, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('bouquet-sold-number').innerText = formattedNumber
     })
     
-    animateNumber(1500, 10, 0, function (number) {
-      const formattedNumber = number
-      document.getElementById('customer-count').innerText = formattedNumber
+    animateNumber(7000, 10, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('happy-clients-number').innerText = formattedNumber
+    })
+
+    animateNumber(10, 500, 0, function (number) {
+      const formattedNumber = number.toLocaleString()
+      document.getElementById('year-of-exp-number').innerText = formattedNumber
     })
   })
