dbf4947a0a9607696b130f1ba046215187a9edd2
PhamBaVan <bavanpham2000@gmail.com>
2022-07-29 17:53:55 +0700

    modified 17:53 29/07/2022

--- infor_page/js/infor_page.js
+++ infor_page/js/infor_page.js
@@ -1,17 +1,17 @@
-function myFunction() {
-    document.getElementById("btn-dropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('btn-dropdown')) {
-      var dropdowns = document.getElementsByClassName("dropdown-container");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-    }
-  }
+// dropdown list infor_page
+var flip = 0, flip_2 = 0, flip_3 = 0, flip_4 = 0;
+    $( '.dropdown-choose-1' ).click(function() {
+      $( '.paragraph-one').toggle(flip++ % 2 === 0);
+    });
+
+    $( '.dropdown-choose-2' ).click(function() {
+      $( '.paragraph-two').toggle(flip_2++ % 2 === 0);
+    });
+
+    $( '.dropdown-choose-3' ).click(function() {
+      $( '.paragraph-three').toggle(flip_3++ % 2 === 0);
+    });
+
+    $( '.dropdown-choose-4' ).click(function() {
+      $( '.paragraph-four').toggle(flip_4++ % 2 === 0);
+    });
