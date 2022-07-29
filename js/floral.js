5087b545800846f5dfe00577421740ba4ddb5bd5
PhamBaVan <bavanpham2000@gmail.com>
2022-07-29 15:11:53 +0700

    upto remote 15:11 29/07/2022

--- js/floral.js
+++ js/floral.js
@@ -1,3 +1,48 @@
+$('.respon-slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 200,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true,
+        prevArrow:`<button type='button' class='slick-prev pull-left'><i class="fa-solid fa-arrow-left"></i></button>`,
+        nextArrow:`<button type='button' class='slick-next pull-right'><i class="fa-solid fa-arrow-right"></i></button>`,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+        breakpoint: 784,
+        settings:{
+            slidesToShow: 2,
+            slidesToScroll: 1
+        }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
 
 const click_sidebar = document.querySelector(".js-open-sidebar");
 const sidebar_overlay = document.querySelector(".js-sidebar-overlay");
