b94c1ee6cebb3d04d0400216762ece86009497ac
QuangHuy <huy2kbg@gmail.com>
2022-08-19 00:28:04 +0700

    huynq1

---
+++ Lalisa-page/js/slide1.js
@@ -0,0 +1,7 @@
+var swiper = new Swiper(".mySwiper1", {
+        slidesPerView: 2,
+        centeredSlides: true,
+        centeredSlidesBounds : true,
+        loop : true,
+        slideToClickedSlide: true
+      });
