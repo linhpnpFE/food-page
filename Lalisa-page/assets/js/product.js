576df1c4fa15e5a80780e419f608a4dc855bdb58
QuangHuy <huy2kbg@gmail.com>
2022-08-18 18:20:15 +0700

    huynq1

---
+++ Lalisa-page/assets/js/product.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('.banner_list_product').slick({
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../assets/img/prev_btn.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../assets/img/next_btn.png'>"
+  });
+})
