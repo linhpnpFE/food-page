f8d90f410e846166563021734b11d309bf1871c3
@binhvl <binhvl@newwave.vn>
2022-08-19 14:25:51 +0700

    article page(1/4 màn pc)

---
+++ Lalisa-page/assets/js/article.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('.list_article').slick({
+        prevArrow:"<img class='a-left control-c prev slick-prev' src='../assets/img/prev_btn.png'>",
+        nextArrow:"<img class='a-right control-c next slick-next' src='../assets/img/next_btn.png'>"
+    });
+})
