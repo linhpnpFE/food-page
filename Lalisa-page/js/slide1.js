14f22f06286634891331f39448548151bc05a392
QuangHuy <huy2kbg@gmail.com>
2022-08-19 15:06:06 +0700

    huynq-html

--- Lalisa-page/js/slide1.js
+++ Lalisa-page/js/slide1.js
@@ -1,7 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+      centeredSlides: true,
+      centeredSlidesBounds : true,
+      loop : true,
+      slideToClickedSlide: true,
+      spaceBetween: 40,
+    });
+    
 var swiper = new Swiper(".mySwiper1", {
         slidesPerView: 2,
         centeredSlides: true,
         centeredSlidesBounds : true,
         loop : true,
         slideToClickedSlide: true
-      });
+});
+
+var swiper = new Swiper(".mySwiper2", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
