f009f439d2ebce1705028f025ed96765a700c0c3
@binhvl <binhvl@newwave.vn>
2022-08-22 17:35:01 +0700

    Article Page

--- Lalisa-page/assets/js/article.js
+++ Lalisa-page/assets/js/article.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
     $('.list_article').slick({
         prevArrow:"<img class='a-left control-c prev slick-prev' src='../assets/img/prev_btn.png'>",
-        nextArrow:"<img class='a-right control-c next slick-next' src='../assets/img/next_btn.png'>"
+        nextArrow:"<img class='a-right control-c next slick-next' src='../assets/img/next_btn.png'>",
+        responsive: [ {
+            breakpoint: 1200,
+            settings: {
+                arrows: false,
+                auto:true,
+            },
+        }],
     });
 })
