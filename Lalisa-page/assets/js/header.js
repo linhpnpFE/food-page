858faa03f2016f9946e5b10210e3d1c9782fc164
@binhvl <binhvl@newwave.vn>
2022-08-26 13:53:03 +0700

    Update all code

--- Lalisa-page/assets/js/header.js
+++ Lalisa-page/assets/js/header.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     $("#open_button").click(function(){
         $("#mySidenav").css("width","50%");
-        $("body").css("background-color","rgba(0,0,0,0.4)")
+        $("body").css({"background-color":"rgba(0,0,0,0.4)","transition":"0.5s;"})
     });
 
     $("main,.close_icon").click(function(){
         $("#mySidenav").css("width","0%");
-        $("body").css("background-color","white")
+        $("body").css({"background-color":"white","transition": "0.5s"})
     })
 });
+
--- Lalisa-page/assets/js/index.js
+++ Lalisa-page/assets/js/index.js
@@ -1,31 +1,15 @@
 $(document).ready(function () {
 
-    $.fn.isInViewport = function() {
-        var elementTop = $(this).offset().top;
-        var elementBottom = elementTop + $(this).outerHeight();
-    
-        var viewportTop = $(window).scrollTop();
-        var viewportBottom = viewportTop + $(window).height();
-    
-        return elementBottom > viewportTop && elementTop < viewportBottom;
-    };
-
-    $(window).on('resize scroll', function() {
-        if ($('.block_our_product').isInViewport()) {
-
-            $(".title_text").each(function() {
-                var $this = $(this);
-                $({ Counter: 0 }).animate({ Counter: $this.attr('data-stop') } , 
-                {
-                    duration: 1000,
-                    easing: 'swing',
-                    step: function (now) {
-                        $this.text(Math.ceil(now)+ "+");
-                    }
-                });
-            });
-        
-        }
+    $(".title_text").each(function() {
+        var $this = $(this);
+        $({ Counter: 0 }).animate({ Counter: $this.attr('data-stop') } , 
+        {
+            duration: 1000,
+            easing: 'swing',
+            step: function (now) {
+                $this.text(Math.ceil(now)+ "+");
+            }
+        });
     });
 
     
