ae884a241b9db4442725c4326425c818e169e766
@binhvl <binhvl@newwave.vn>
2022-08-29 17:48:42 +0700

    Fix header

--- Lalisa-page/assets/js/header.js
+++ Lalisa-page/assets/js/header.js
@@ -1,12 +1,11 @@
 $(document).ready(function(){
     $("#open_button").click(function(){
-        $("#mySidenav").css("width","50%");
-        $("body").css({"background-color":"rgba(0,0,0,0.4)","transition":"0.5s;"})
+        $("#mySidenav").css("width","320px");
+        $("body").css({"background-color":"rgba(0,0,0,0.4)","transition":"0.5s","overflow-y":"hidden"})
     });
 
     $("main,.close_icon").click(function(){
         $("#mySidenav").css("width","0%");
-        $("body").css({"background-color":"white","transition": "0.5s"})
+        $("body").css({"background-color":"white","transition": "0.5s","overflow-y":"unset"})
     })
 });
-
