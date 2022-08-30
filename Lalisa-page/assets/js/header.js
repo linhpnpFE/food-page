625289799b8c67093cecfc09a50883cf8740792f
@binhvl <binhvl@newwave.vn>
2022-08-30 09:48:12 +0700

    fix background cover when navbar mobile show

--- Lalisa-page/assets/js/header.js
+++ Lalisa-page/assets/js/header.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     $("#open_button").click(function(){
         $("#mySidenav").css("width","320px");
-        $("body").css({"background-color":"rgba(0,0,0,0.4)","transition":"0.5s","overflow-y":"hidden"})
+        $(".cover_body").css({
+            "display":"block",
+            "transition":"0.5s",
+        });
+        $("body").css({"transition":"0.5s","overflow-y":"hidden"})
     });
 
     $("main,.close_icon").click(function(){
         $("#mySidenav").css("width","0%");
-        $("body").css({"background-color":"white","transition": "0.5s","overflow-y":"unset"})
+        $(".cover_body").css({"display":"none"});
+        $("body").css({"overflow-y":"unset"})
     })
 });
