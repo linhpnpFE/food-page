c0ccc6bd709313fd1e7ee1e8d389a0e23bee6ad2
Ngantt0093 <ngothunga0093@gmail.com>
2022-12-29 09:38:13 +0700

    update

---
+++ ngantt-italian-food/assets/js/index.js
@@ -0,0 +1,10 @@
+$('.menu__img-slider').slick({
+    centerMode: true,
+    centerPadding: '30px',
+    slidesToShow: 3,
+    adaptiveHeight: false,
+    infinite: true,
+    arrows: false,
+    dots: true
+});
+    
